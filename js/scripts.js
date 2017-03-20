64d956ce8365ea8bdfc35913c797281067025f4d
Asia Kane and Spencer Ruiz <spencer@>
2017-03-20 15:57:42 -0700

    adds jquery, html images, and clickable enctyption

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,60 @@
-// Prompt user to enter sentence
-var sentence = prompt("Please enter a sentence.");
+$(document).ready(function() {
 
-//Create Uppercase first and last letter
-var firstLastUpper = function(sentence){
+  // Prompt user to enter sentence
+  var sentence = prompt("Please enter a sentence.");
 
-  var firstLast = sentence.charAt(0) + sentence.charAt(sentence.length - 1);
-  var firstLastUpper = firstLast.toUpperCase();
-  return firstLastUpper;
+  //Create Uppercase first and last letter
+  var firstLastUpper = function(sentence){
 
-};
+    var firstLast = sentence.charAt(0) + sentence.charAt(sentence.length - 1);
+    var firstLastUpper = firstLast.toUpperCase();
+    return firstLastUpper;
 
-//Create variable for reverse first and last letter
-var reverseString = function(string) {
+  };
 
-  var firstLastArray = string.split("");
-  var reverseArray = firstLastArray.reverse();
-  var joinArray = reverseArray.join("");
-  return joinArray;
+  //Create variable for reverse first and last letter
+  var reverseString = function(string) {
 
-};
+    var firstLastArray = string.split("");
+    var reverseArray = firstLastArray.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
 
-//Third function
-var concatAtEnd = function(sentence1) {
+  };
 
-  var firstLastString = firstLastUpper(sentence1);
-  var reverseFirstLastString = reverseString(firstLastString);
-  return sentence1 + reverseFirstLastString;
+  //Third function
+  var concatAtEnd = function(sentence1) {
 
-};
+    var firstLastString = firstLastUpper(sentence1);
+    var reverseFirstLastString = reverseString(firstLastString);
+    return sentence1 + reverseFirstLastString;
 
-var sentenceWithEnd = concatAtEnd(sentence);
+  };
 
-console.log(sentence);
-console.log(sentenceWithEnd);
+  var sentenceWithEnd = concatAtEnd(sentence);
 
-//Fourth function
+  //Fourth function
 
-var concatAtBeg = function(sentence, sentenceWithEnd) {
+  var concatAtBeg = function(sentence, sentenceWithEnd) {
 
-  var middleNumber = (Math.floor(sentence.length / 2));
-  var charAtMiddleNumber = sentence.charAt(middleNumber);
-  return charAtMiddleNumber + sentenceWithEnd;
+    var middleNumber = (Math.floor(sentence.length / 2));
+    var charAtMiddleNumber = sentence.charAt(middleNumber);
+    return charAtMiddleNumber + sentenceWithEnd;
 
-};
+  };
 
-console.log(concatAtBeg(sentence, sentenceWithEnd));
+  var encodeSentence = reverseString(concatAtBeg(sentence, sentenceWithEnd));
+
+
+//Running Stuff
+
+  $("#original-img").click(function(){
+    alert(sentence);
+  });
+
+  $("#encrypted-img").click(function(){
+    alert(encodeSentence);
+  });
+
+
+});
