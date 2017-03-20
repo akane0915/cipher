c1c77cc91b8915be806e023b4794f63678e01aca
Asia Kane and Spencer Ruiz <asia@>
2017-03-20 15:28:29 -0700

    Create third and fourth functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,46 @@
 var sentence = prompt("Please enter a sentence.");
 
 //Create Uppercase first and last letter
-var firstLast = sentence.charAt(0) + sentence.charAt(sentence.length - 1);
+var firstLastUpper = function(sentence){
 
-var firstLastUpper = firstLast.toUpperCase();
+  var firstLast = sentence.charAt(0) + sentence.charAt(sentence.length - 1);
+  var firstLastUpper = firstLast.toUpperCase();
+  return firstLastUpper;
+
+};
 
 //Create variable for reverse first and last letter
-var reverseFirstLast = function(string) {
+var reverseString = function(string) {
 
   var firstLastArray = string.split("");
-
   var reverseArray = firstLastArray.reverse();
-
   var joinArray = reverseArray.join("");
-
   return joinArray;
 
 };
 
+//Third function
+var concatAtEnd = function(sentence1) {
+
+  var firstLastString = firstLastUpper(sentence1);
+  var reverseFirstLastString = reverseString(firstLastString);
+  return sentence1 + reverseFirstLastString;
+
+};
+
+var sentenceWithEnd = concatAtEnd(sentence);
+
 console.log(sentence);
-console.log(firstLastUpper);
-console.log(reverseFirstLast(firstLastUpper));
+console.log(sentenceWithEnd);
+
+//Fourth function
+
+var concatAtBeg = function(sentence, sentenceWithEnd) {
+
+  var middleNumber = (Math.floor(sentence.length / 2));
+  var charAtMiddleNumber = sentence.charAt(middleNumber);
+  return charAtMiddleNumber + sentenceWithEnd;
+
+};
+
+console.log(concatAtBeg(sentence, sentenceWithEnd));
